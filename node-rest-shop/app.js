2cc63ccc11c7da1b958fec5b2c587ee8a990fc75
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-03-02 22:03:54 -0800

    CORS

--- node-rest-shop/app.js
+++ node-rest-shop/app.js
@@ -12,6 +12,16 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+
+});
 
 // Routes which handle requests
 app.use('/products', productRoutes);
