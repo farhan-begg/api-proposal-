b46f417ea2e895617fb7f3ef538a49ca81bd5a61
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-03-03 14:37:22 -0800

    queries

--- node-rest-shop/api/routes/products.js
+++ node-rest-shop/api/routes/products.js
@@ -1,14 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+const Product = require("../models/product");
 
-const Product = require('../models/product');
-
-
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
     Product.find()
-        .select('name price_id')
+        .select("name price _id")
         .exec()
         .then(docs => {
             const response = {
@@ -19,74 +17,63 @@ router.get('/', (req, res, next) => {
                         price: doc.price,
                         _id: doc._id,
                         request: {
-                            type: 'GET',
-                            url: 'http://localhost:3000/products/' + doc._id
+                            type: "GET",
+                            url: "http://localhost:3000/products/" + doc._id
                         }
-
-                    }
+                    };
                 })
-
             };
-
-            // if (docs.length >= 0) {
+            //   if (docs.length >= 0) {
             res.status(200).json(response);
-
-            // } else {
-            //     res.status(404).json({
-            //         message: 'No entries found'
-            //     });
-
-            // }
-
+            //   } else {
+            //       res.status(404).json({
+            //           message: 'No entries found'
+            //       });
+            //   }
         })
         .catch(err => {
             console.log(err);
             res.status(500).json({
                 error: err
-
             });
         });
-
 });
 
-
-router.post('/', (req, res, next) => {
+router.post("/", (req, res, next) => {
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
     });
-    product.save().then(result => {
+    product
+        .save()
+        .then(result => {
             console.log(result);
             res.status(201).json({
-                message: 'Created product successfully',
+                message: "Created product successfully",
                 createdProduct: {
                     name: result.name,
                     price: result.price,
                     _id: result._id,
                     request: {
                         type: 'GET',
-                        url: 'http://localhost:3000/products/' + result._id
+                        url: "http://localhost:3000/products/" + result._id
                     }
-
                 }
-
             });
         })
         .catch(err => {
             console.log(err);
             res.status(500).json({
                 error: err
-            })
+            });
         });
-
-
 });
 
-router.get('/:productId', (req, res, next) => {
+router.get("/:productId", (req, res, next) => {
     const id = req.params.productId;
     Product.findById(id)
-        .select('name price')
+        .select('name price _id')
         .exec()
         .then(doc => {
             console.log("From database", doc);
@@ -95,30 +82,26 @@ router.get('/:productId', (req, res, next) => {
                     product: doc,
                     request: {
                         type: 'GET',
-                        description: 'Get all products',
-                        url: 'http://localhost:3000/products/'
+                        url: 'http://localhost:3000/products'
                     }
-
                 });
             } else {
-                res.status(404).json({
-                    message: 'No valid entry found for provided ID '
-                });
+                res
+                    .status(404)
+                    .json({
+                        message: "No valid entry found for provided ID"
+                    });
             }
-
-
         })
         .catch(err => {
             console.log(err);
             res.status(500).json({
-                err: err
+                error: err
             });
         });
-
-
 });
 
-router.patch('/:productId', (req, res, next) => {
+router.patch("/:productId", (req, res, next) => {
     const id = req.params.productId;
     const updateOps = {};
     for (const ops of req.body) {
@@ -131,7 +114,6 @@ router.patch('/:productId', (req, res, next) => {
         })
         .exec()
         .then(result => {
-
             res.status(200).json({
                 message: 'Product updated',
                 request: {
@@ -144,41 +126,35 @@ router.patch('/:productId', (req, res, next) => {
             console.log(err);
             res.status(500).json({
                 error: err
-            })
+            });
         });
-
-
 });
 
-
-router.delete('/:productId', (req, res, next) => {
+router.delete("/:productId", (req, res, next) => {
     const id = req.params.productId;
-    Product.deleteOne({
+    Product.remove({
             _id: id
-        }).exec()
+        })
+        .exec()
         .then(result => {
             res.status(200).json({
                 message: 'Product deleted',
                 request: {
                     type: 'POST',
-                    url: 'http://localhost:3000/products/',
+                    url: 'http://localhost:3000/products',
                     body: {
                         name: 'String',
                         price: 'Number'
                     }
-
                 }
-            })
+            });
         })
         .catch(err => {
             console.log(err);
             res.status(500).json({
                 error: err
-            })
-
+            });
         });
-
 });
-
 
 module.exports = router;
