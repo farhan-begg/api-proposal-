813e18e8fec7529f3f57222664f8f5e5a280dd40
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-03-02 23:28:48 -0800

    product routes

--- node-rest-shop/app.js
+++ node-rest-shop/app.js
@@ -2,15 +2,24 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://Farhan-Begg:' + process.env.MONGO_ATLAS_PW + '@cluster0-p5jdm.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+});
+
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
+
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
